467714b2cba7e3b7c0ebac987bb0f4a6855822a7
Chirla Surya Sampreeth <cb.sc.u4cse23410@cb.students.amrita.edu>
2025-11-25 23:55:20 +0530

    Updated __init__.py
    Changes made wrt the task api with predict and result endpoint with model running in the backend

--- backend/src/preprocessing/__init__.py
+++ backend/src/preprocessing/__init__.py
@@ -6,5 +6,6 @@ Milestone 1 (Weeks 1-2)
 from .data_loader import DataLoader
 from .data_cleaner import DataCleaner
 #from .feature_engineer import FeatureEngineer
+from .preprocessor import FraudPreprocessor
 
 __all__ = ['DataLoader', 'DataCleaner', 'FeatureEngineer']
